b932e4f5a717d0510fa6dcacca9700cd167ab31a
unknown <chris.prodan7@gmail.com>
2026-09-08 12:36:09 -0400

    improved readability

--- backend/app.py
+++ backend/app.py
@@ -1,71 +1,81 @@
 import os
-from flask import Flask
+from flask import Flask , jsonify
 from flask_cors import CORS
-from flask import jsonify 
 import psycopg
 
-from dotenv import load_dotenv
-
-load_dotenv()
-password = os.environ.get("DB_PASSWORD")
+import dp
+import config
 
 app = Flask(__name__)
 CORS(app)
 
-#Returns set ID from name, -1 if failure
-def getSet(setName):
+# Suggests a List of Sets Based on Partial String
+@app.route("/suggest/sets/<path:setName>")
+def SuggestSet(setName):
+    if setName == "":
+        return jsonify({"suggestions":[]})
+    else:
+        pattern = f"%{setName}%"
+        with dp.get_connection(config.load_settings) as conn:
+
+            with conn.cursor() as cur:
+                try:
+                    cur.execute(
+                        t"SELECT setname FROM groupdata WHERE setname ILIKE {pattern} LIMIT 5"
+                    )
+                    return jsonify({"suggestions":[x[0] for x in cur.fetchall()]})
+                except psycopg.Error as e:
+                    return jsonify({"suggestions":[]})
+
+    return
+
+# Suggests a List of Cards Based on Partial String
+@app.route("/suggest/card/<path:setName>/<path:cardName>")
+def SuggestCard(setName,cardName):
+    setId = dp.getSet(dp.get_connection(config.load_settings), setName)
     
-    with psycopg.connect(dbname="Pokemon_Pricing_Information",
-                                         user="postgres",
-                                         password=password,
-                                         port=5432,
-                                         host="localhost",
-                                         autocommit=True) as conn:
-                        
-                with conn.cursor() as cur:
-                    try:
-                        cur.execute(
-                            t"SELECT groupid FROM groupdata WHERE setname={setName}"
-                        )
-                        
-                        retval = cur.fetchone()
-                        if retval is None:
-                            return -1
-                        return retval[0]
-                        
-                    except psycopg.Error as e:
-                        return -1
+    if setId == -1:
+        return jsonify({"suggestions":[]})
+    else:
+        pattern = f"%{cardName}%"
+        with dp.get_connection(config.load_settings) as conn:
+
+            with conn.cursor() as cur:
+
+                try:
+                    cur.execute(
+                        t"SELECT cardname FROM carddata WHERE groupid={setId} AND cardname ILIKE {pattern} LIMIT 5"
+                    )
+                    return jsonify({"suggestions":[x[0] for x in cur.fetchall()]})
+                except psycopg.Error as e:
+                    return jsonify({"suggestions":[]})
+
 
 # Returns Card imageUrl and Price given setName and cardName
 @app.route("/<path:setName>/<path:cardName>")        
 def getCardInfo(setName, cardName):
-    groupId = getSet(setName)
+    groupId = dp.getSet(dp.get_connection(config.load_settings), setName)
     
     if groupId == -1:
         return jsonify({"error": "not found"}), 404
     
-    with psycopg.connect(dbname="Pokemon_Pricing_Information",
-                                             user="postgres",
-                                             password=password,
-                                             port=5432,
-                                             host="localhost",
-                                             autocommit=True) as conn:
+    with dp.get_connection(config.load_settings) as conn:
                             
-                    with conn.cursor() as cur:
-                        try:
-                            cur.execute(
-                                t"SELECT imageurl, price FROM carddata WHERE groupid={groupId} AND cardname={cardName}"
-                            )
-                            
-                            r = cur.fetchone()  
-                            if r is None:
-                                return jsonify({"error": "not found"}), 404                                                 
-                            retval = jsonify({"imageUrl":r[0],"price":r[1]})
-                            
-                            return retval
-                            
-                        except psycopg.Error as e:
-                            return jsonify({"error": "not found"}), 404
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    t"SELECT imageurl, price FROM carddata WHERE groupid={groupId} AND cardname={cardName}"
+                )
+                
+                r = cur.fetchone()  
+                if r is None:
+                    return jsonify({"error": "not found"}), 404                                                 
+                retval = jsonify({"imageUrl":r[0],"price":f"{r[1]:.2f}"})
+                
+                return retval
+                
+            except psycopg.Error as e:
+                return jsonify({"error": "not found"}), 404
     
     
 
@@ -79,4 +89,4 @@ def main():
 
 if __name__ == "__main__":
     # Starts the server instantly when the script is run directly
-    app.run()
+    app.run(debug=True)
